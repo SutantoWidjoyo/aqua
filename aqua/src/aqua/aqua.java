adc0a07ae139e5e77bf3435ea54ba3ead1a8e9ab
SutantoWidjoyo <sutanto_widjoyo@yahoo.com>
2015-11-25 15:54:30 +0700

    reset
    reset

--- aqua/src/aqua/aqua.java
+++ aqua/src/aqua/aqua.java
@@ -3,14 +3,7 @@ package aqua;
 public class aqua {
 
 	public static void main(String[] args) {
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
-		System.out.println("a");
-		System.out.println("b");
-		System.out.println("c");
-		System.out.println("d");
+	
 	}
 
 }
