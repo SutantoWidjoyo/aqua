36a0e3013cbe486d3daaeec457971a3c07451846
Sutanto lim <sutanto_widjoyo@sofcograha.co.id>
2018-12-06 14:23:08 +0700

    my features
    mytest1
    
    mytest2
    
    master rubah
    
    mytest3

--- aqua/src/aqua/aqua.java
+++ aqua/src/aqua/aqua.java
@@ -3,22 +3,12 @@ package aqua;
 public class aqua {
 
 	public static void main(String[] args) {
-		System.out.println("dd");
-		System.out.println("1234");
+		System.out.println("xyz");
 	}
 	
 	private void method1() {
 		System.out.println("1234");
 	}
 	
-	private void method2() {
-		System.out.println("1234");
-	}
 	
-	private void method3() {
-		System.out.println("dd");
-	}
-	private void method4() {
-		System.out.println("dd");
-	}
 }
