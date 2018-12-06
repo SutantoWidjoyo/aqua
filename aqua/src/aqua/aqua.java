fc576e440009f58e7ad154619934186e2168e21d
Sutanto lim <sutanto_widjoyo@sofcograha.co.id>
2018-12-06 13:46:46 +0700

    test 3 final

--- aqua/src/aqua/aqua.java
+++ aqua/src/aqua/aqua.java
@@ -3,9 +3,6 @@ package aqua;
 public class aqua {
 
 	public static void main(String[] args) {
-		System.out.println("aa");
-		System.out.println("bb");
-		System.out.println("cc");
 		System.out.println("dd");
 		System.out.println("1234");
 	}
